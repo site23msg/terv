916dc8f62111516b68d2f812b9d911bed1fe9cdf
Galaczi Arnold <sitebuild@23msg.com>
2023-02-14 09:07:47 +0100

    update1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      backgroundImage: {
+        'hero-pattern': "url('./assets/hero.png')",
+      },
+      boxShadow: {
+        '3xl': '-0px 30px 60px 20px rgba(0, 0, 0, 1)',
+      }
     },
   },
   plugins: [],
